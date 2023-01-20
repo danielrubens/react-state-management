3bdb913d61ff64dca209f191c61e1712b09ff23d
danielrubens <danielrubensdaniel@gmail.com>
2023-01-20 01:28:04 -0300

    arrow function on useState to have a initial value after refresh

--- native-useState/src/App.jsx
+++ native-useState/src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 function Counter({value}) {
   let [count, setCount] = useState(10)
   const addOne = () => setCount(count+1)
+  const [name, setName] = useState(() => Jack)
+  // arrow function on useState to have a initial value after refresh 
   // setter enqueue a request to rerender the component
   return (
     <div className="App">
