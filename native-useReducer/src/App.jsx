22a674958ba65d54dd6de4c8f66fbb2fae17ed5d
danielrubens <danielrubensdaniel@gmail.com>
2023-01-20 01:55:56 -0300

    useReducer adding names to an array

--- native-useReducer/src/App.jsx
+++ native-useReducer/src/App.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useReducer } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer((state, action) => {
+    switch(action.type){
+      case 'SET_NAME':
+        return {...state, name: action.payload}
+      case 'ADD_NAME':
+        return {...state, names:[...state.names, state.name], name:''}
+    }
+  }, { names: [], name: "" })
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {state.names.map((i, j) => (
+           <div key={j}>{i}</div>
+           ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input 
+        type="text"
+        value={state.name}
+        onChange={e => dispatch({type: 'SET_NAME', payload: e.target.value})}
+      />
+      <button
+        onClick={() => dispatch({type: 'ADD_NAME'})}
+      >Add Name
+      </button>
     </div>
   )
 }
