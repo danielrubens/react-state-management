c020d399605d9fbd616f63ddde7825132d430869
danielrubens <danielrubensdaniel@gmail.com>
2023-02-03 03:05:15 -0300

    spread array to sort

--- native-useMemo-useCallback/src/App.jsx
+++ native-useMemo-useCallback/src/App.jsx
@@ -6,10 +6,14 @@ import React, {useState, useMemo} from 'react'
 function App() {
   const [numbers, setNumbers] = useState([10,20,30])
   const total = useMemo(() => numbers.reduce((a,b) => a+b, 0), [numbers])
-  const names = useState(["John", "Paul", "George", "Ringo"])
+  const [names] = useState(["John", "Paul", "George", "Ringo"])
+
+  const sortedNames = [...names].sort() //copy names, then sort
   return (
     <div>
       {`Total: ${total}`}
+      {`Names: ${names.join(", ")}`}
+      {`Sorted: ${sortedNames.join(", ")} `}
     </div>
   )
 }
