9ca9fa5284e5bd9c85b8f7ff2cc91540a528fd3d
danielrubens <danielrubensdaniel@gmail.com>
2023-02-03 00:20:08 -0300

    useMemo init

--- native-useMemo-useCallback/src/App.jsx
+++ native-useMemo-useCallback/src/App.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useState, useMemo} from 'react'
+
+//hooks to monitor state
+// useMemo takes a function and that functions does the calculation
 
 function App() {
-
+  const [numbers, setNumbers] = useState([10,20,30])
+  const total = useMemo(() => numbers.reduce((a,b) => a+b, 0), [numbers])
+  const names = useState(["John", "Paul", "George", "Ringo"])
   return (
     <div>
-      Hello
+      {`Total: ${total}`}
     </div>
   )
 }
