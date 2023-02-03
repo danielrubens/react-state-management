48bf1925f8c69edaa5ce128ca3c67a2a987d67e9
danielrubens <danielrubensdaniel@gmail.com>
2023-02-03 03:15:47 -0300

    SortedList component

--- native-useMemo-useCallback/src/App.jsx
+++ native-useMemo-useCallback/src/App.jsx
@@ -3,17 +3,20 @@ import React, {useState, useMemo} from 'react'
 //hooks to monitor state
 // useMemo takes a function and that functions does the calculation
 
+function SortedList({ list }){
+  const sortedList = useMemo(() => [...list].sort(), [list])
+  return (<div>{sortedList.join(", ")}</div>)
+  
+}
+
 function App() {
-  const [numbers, setNumbers] = useState([10,20,30])
+  const [numbers] = useState([10,20,30])
   const total = useMemo(() => numbers.reduce((a,b) => a+b, 0), [numbers])
   const [names] = useState(["John", "Paul", "George", "Ringo"])
 
-  const sortedNames = [...names].sort() //copy names, then sort
   return (
     <div>
-      {`Total: ${total}`}
-      {`Names: ${names.join(", ")}`}
-      {`Sorted: ${sortedNames.join(", ")} `}
+      <SortedList list={names} />
     </div>
   )
 }
