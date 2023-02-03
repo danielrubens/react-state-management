2285deb52604906c0dd1c3078343b92d7b53de0e
danielrubens <danielrubensdaniel@gmail.com>
2023-02-03 03:28:43 -0300

    useCallback avoindin rerun

--- native-useMemo-useCallback/src/App.jsx
+++ native-useMemo-useCallback/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react'
+import React, {useState, useMemo, useCallback} from 'react'
 
 // hooks to monitor state
 // useMemo takes a function and that functions does the calculation
@@ -8,13 +8,13 @@ function SortedList({ list, sortFunc }){
   return (<div>{sortedList.join(", ")}</div>)
 }
 
-const sortFunc = (a,b) => a.localeCompare(b) * -1
 
 function App() {
   const [numbers] = useState([10,20,30])
   const total = useMemo(() => numbers.reduce((a,b) => a+b, 0), [numbers])
   const [names] = useState(["John", "Paul", "George", "Ringo"])
-
+  
+  const sortFunc = useCallback((a,b) => a.localeCompare(b) * -1, [])
   return (
     <div>
       <SortedList list={names} sortFunc={sortFunc} />
