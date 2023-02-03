2466fc00f7f8c05e895b46a1811231cc63242d78
danielrubens <danielrubensdaniel@gmail.com>
2023-02-03 03:21:05 -0300

    localeCompare and sortFunc on list of dependencies

--- native-useMemo-useCallback/src/App.jsx
+++ native-useMemo-useCallback/src/App.jsx
@@ -1,22 +1,22 @@
 import React, {useState, useMemo} from 'react'
 
-//hooks to monitor state
+// hooks to monitor state
 // useMemo takes a function and that functions does the calculation
 
-function SortedList({ list }){
-  const sortedList = useMemo(() => [...list].sort(), [list])
+function SortedList({ list, sortFunc }){
+  const sortedList = useMemo(() => [...list].sort(sortFunc), [list, sortFunc])
   return (<div>{sortedList.join(", ")}</div>)
-  
 }
 
 function App() {
   const [numbers] = useState([10,20,30])
   const total = useMemo(() => numbers.reduce((a,b) => a+b, 0), [numbers])
   const [names] = useState(["John", "Paul", "George", "Ringo"])
+  const sortFunc = (a,b) => a.localeCompare(b) * -1
 
   return (
     <div>
-      <SortedList list={names} />
+      <SortedList list={names} sortFunc={sortFunc} />
     </div>
   )
 }
