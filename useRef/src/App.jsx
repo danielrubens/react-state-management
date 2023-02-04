d42d4351ced8decfce12c351b4a4de1892459c1b
danielrubens <danielrubensdaniel@gmail.com>
2023-02-04 15:17:47 -0300

    autoincrement uid

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -5,18 +5,27 @@ function App() {
   const inputRef = useRef(null)
 
   useEffect(() => {inputRef.current.focus()}, [])
+  
+  const idRef = useRef(1)
+  const [names, setNames] = useState([
+    {id: idRef.current++, name:"John"  }, {id: idRef.current++, name: "Jane"}
+  ])
 
   const onAddName = () =>{
-    setNames([...names, inputRef.current.value])
+    setNames([...names, 
+      {
+      id: idRef.current++,
+      name: inputRef.current.value
+    }
+    ])
     inputRef.current.value = ''
 
   }
 
-  const [names, setNames] = useState([])
   return (
     <div>
       <div>
-        {names.map((name, index) => (<p key={index}>{name}</p>))}
+        {names.map((name, index) => (<p key={index}>{`id: ${name.id} Name: ${name.name}`}</p>))}
       </div>
       <input type="text" ref={inputRef}/>
       <buton
