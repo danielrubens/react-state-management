b0d1b216545142ab5dbb2cc91d25183a15ad42d7
danielrubens <danielrubensdaniel@gmail.com>
2023-02-04 15:43:47 -0300

    interface Pokemon

--- Context/src/App.tsx
+++ Context/src/App.tsx
@@ -1,4 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+const usePokemon = () => {
+  const [pokemon, setPokemon] = useState([])
+  useEffect(() => {
+    fetch('./pokemon.json').then((response) => response.json()).then((data) => setPokemon(data))
+  }, [])
+  return {pokemon}
+
+}
 
 function App() {
   const [count, setCount] = useState(0)
