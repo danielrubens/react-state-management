89bb4471f3bdeb0d25aeb4cde8e47b6151e7d426
danielrubens <danielrubensdaniel@gmail.com>
2023-02-04 01:59:26 -0300

    dinamically reading json files

--- native-useEffect/src/App.jsx
+++ native-useEffect/src/App.jsx
@@ -3,18 +3,28 @@ import { useState, useEffect } from 'react'
 function App() {
   const [names, setNames] = useState([])
   const [data, setData] = useState([])
-
+  
   // useEffect takes a function and call once the DOM is rendered or
   // when the dependencie array changes
   
   useEffect(() => {
     fetch("/names.json").then((response) => response.json().then((data) => setNames(data)))
   }, [])
+  
+  const [selectedName, setSelectName] = useState(null)
+  const [selectedNameDetails, setSelectNameDetails] = useState(null)
+  useEffect(() => {
+    fetch(`/${selectedName}.json`).then((response) => response.json().then((data) => setSelectNameDetails(data)))
+  }, [selectedName])
 
   return (
     <div className="App">
-      <div>Names: {names.join(", ")}</div>
+      <div>{names.map((name) => (
+          <button onClick={() => setSelectName(name)}>{name}</button>))}
+      </div>
+      <div>{JSON.stringify(selectedNameDetails)}</div>
     </div>
+
   )
 }
 
